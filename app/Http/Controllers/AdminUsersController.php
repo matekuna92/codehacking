e4c74a0ada742fef6aef16e2d8985d27ec55021e
Mate <kunamate66@gmail.com>
2016-12-17 13:24:31 +0100

    users with photos working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\User;
 use App\Role;
+use App\Photo;
 use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
@@ -49,13 +50,31 @@ class AdminUsersController extends Controller
         // csak teszteljük, hogy ad e vissza egyáltalán valamit a form, ha a gombra kattintva elküldjük
        // return $request->all();
 
-        User::create($request->all());
-        return redirect('/admin/users');
+        /*User::create($request->all());
+        return redirect('/admin/users');*/
 
         /*Form tesztadatot küld vissza arrayként.... -> php artisan make:request UsersRequest : A requestben megadjuk a
         formhoz szükséges REQUIRED adatokat rules() function-nél, ... majd AdminUsersController-ben a store function mostmár egy
         UsersRequest-et vár.. ez alapján ellenőrzi, megadtunk e mindent a formban, ami required
         FENT IMPORTÁLNI MERT NEM TALÁLJA ANÉLKÜL !!!!! */
+
+        $input = $request->all();
+
+        if ($file = $request->file('photo_id')) // HA VAN KÉP
+            // //a users. create blade-ben át kellett írni az eredeti file-t photo_id-ra hogy megtalálja..
+        {
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name); // public / images folder-be teszi !
+            $photo = Photo::create(['file'=>$name]); // amikor létrehoztuk, ez a photo id elérhető -> ezt használjuk fel a következő sorban
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
+
+            //return "photo exists";
+
 
 
 
--- app/Photo.php
+++ app/Photo.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+    protected $fillable = ['file'];
 
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
+
+    public function photo()
+    {
+        return $this->belongsTo('App\Photo');
+    }
 }
