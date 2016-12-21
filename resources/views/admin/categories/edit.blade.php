bd4058d369f3c7c55ac1b04c1bb5e3e577ae09d1
Mate <kunamate66@gmail.com>
2016-12-21 20:48:35 +0100

    Able to create + EDIT + DELETE categories

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -14,7 +14,15 @@
         </div>
 
         <div class="form-group">
-            {!! Form::submit('Edit category',['class'=>'btn btn-primary']) !!}
+            {!! Form::submit('Edit category',['class'=>'btn btn-primary col-sm-6']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+        {!!Form::open(['method'=>'DELETE', 'action'=>['AdminCategoriesController@destroy',$category->id]]) !!}
+
+        <div class="form-group">
+            {!! Form::submit('Delete category',['class'=>'btn btn-danger col-sm-6']) !!}
         </div>
 
         {!! Form::close() !!}
